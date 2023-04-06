558d4623d5d16c39cf7fe79bf71a620aae884fd3
mike <savvateevmisha@mail.ru>
2023-04-06 23:55:52 +0300

    Добавил проверку лабиринта на проходимость.

--- Game/field.py
+++ Game/field.py
@@ -1,8 +1,10 @@
+from Field_Generate.BFS import is_connected
 from Field_Generate.gen_field import generate_empty_field, generate_basic_borders
 from Field_Generate.gen_exit import generate_exit
 from Field_Generate.gen_river import generate_river
 from Field_Generate.gen_tunnel import generate_tunnel
 from Field_Generate.spanning_tree import generate_walls
+from Cell_Types.water import WaterCell
 from random_generator import randint
 
 
@@ -11,15 +13,24 @@ class Field:
 
     def __init__(self, side_length, **kwargs):
         self.side_length = side_length
-        generate_empty_field(self)
-        generate_basic_borders(self)
-        river_max_len = kwargs[Field.river_name] if Field.river_name in kwargs else side_length
-        generate_river(self, randint(2, river_max_len))
-        tunnel_max_len = kwargs[Field.tunnel_name] if Field.tunnel_name in kwargs else side_length
-        generate_tunnel(self, randint(1, tunnel_max_len))
-        walls_max_num = kwargs[Field.wall_name] if Field.wall_name in kwargs else (side_length-1)**2
-        generate_walls(self, randint(0, walls_max_num))
-        generate_exit(self)
+        while True:
+            generate_empty_field(self)
+            generate_basic_borders(self)
+            river_max_len = kwargs[Field.river_name] if Field.river_name in kwargs else side_length
+            generate_river(self, randint(2, river_max_len))
+            tunnel_max_len = kwargs[Field.tunnel_name] if Field.tunnel_name in kwargs else side_length
+            generate_tunnel(self, randint(1, tunnel_max_len))
+            walls_max_num = kwargs[Field.wall_name] if Field.wall_name in kwargs else (side_length-1)**2
+            generate_walls(self, randint(0, walls_max_num))
+            generate_exit(self)
+            if is_connected(self, self.get_mouth()):
+                break
+
+    def get_mouth(self):
+        for row in self:
+            for cell in row:
+                if isinstance(cell, WaterCell) and cell.type == 2:
+                    return cell
 
     def __str__(self):
         n = self.side_length
